5187237ff0e1e17b34baea6f02bbebe7fc3ecd06
Qusai-alhanaktah <qusaialhanaktah@gmail.com>
2020-01-26 14:59:21 +0200

    sovled the problem

--- classes/__tests__/validator.test.js
+++ classes/__tests__/validator.test.js
@@ -1,38 +1,58 @@
 'use strict';
 
-const validator = require('../validator.js')
+const Validator = require('../validator.js')
 
-
-function isTruthy(input) {
-    if ((input.firstName) && (input.lastName) && (input.hairType) && (input.hairColor) && (input.favoriteFoods) && (input.married) && (input.married) && (input.kids)) return true;
-    else return false;
-};
-function isCorrectType(input) {
-    if (typeof (input.firstName) === 'string' && typeof (input.lastName) === 'string' && typeof (input.hairType) === 'string' && typeof (input.hairColor) === 'string' && typeof (input.favoriteFoods) === 'object' && typeof (input.married) === 'boolean' && typeof (input.married) === 'boolean' && typeof (input.kids) === 'number') return true;
-    else return false;
-
-}
-function type(input) {
-    if (typeof (input.firstName) === 'string') return 'string';
-    if (typeof (input.lastName) === 'string') return 'string';
-    if (typeof (input.hairType) === 'string') return 'string';
-    if (typeof (input.hairColor) === 'string') return 'string';
-    if (typeof (input.favoriteFoods) === 'object') return 'object';
-    if (typeof (input.married) === 'boolean') return 'boolean';
-    if (typeof (input.kids) === 'number') return 'number';
-    else return undefined;
-}
+const schema = {
+      firstName: {type: 'string'},
+      lastName: {type: 'string'},
+      hair: {type: 'object'},
+      favoriteFoods: {type: 'array', valueType: 'string'},
+      married: {type: 'boolean'},
+      kids: {type: 'number'},
+  };
+  
+let str = 'yes';
+let num = 1;
+let arr = ['a'];
+let obj = {x:'y'};
+let func = () => {};
+let bool = false;
 
 describe('validator module', () => {
-    let person = require('../../validator/data/data.json')
-    let validate = new validator(person)
+    let validate = new Validator(schema)
 
-    it('check if the data insert is the same with what should be data type is', () => {
-        expect(isCorrectType(validate)).toBeTruthy();
+    it('check is string', () => {
+        expect(validate.isString(str)).toBeTruthy();
+        expect(validate.isString(num)).toBeFalsy();
+        expect(validate.isString(arr)).toBeFalsy();
+        expect(validate.isString(obj)).toBeFalsy();
+        expect(validate.isString(func)).toBeFalsy();
+        expect(validate.isString(bool)).toBeFalsy();
 
     });
-    it('check if all the data insert is  all the data what should be exist', () => {
-        expect(isTruthy(validate)).toBeTruthy();
+    it('check is number', () => {
+        expect(validate.isNumber(num)).toBeTruthy();
+
+    });
+    it('check is boolean', () => {
+        expect(validate.isBoolean(bool)).toBeTruthy();
+
+    });
+    it('check is object', () => {
+        expect(validate.isObj(obj)).toBeTruthy();
+
+    });
+    it('check is func', () => {
+        expect(validate.isFunc(func)).toBeTruthy();
+
+    });
+    it('check is array', () => {
+        expect(validate.isArray(arr)).toBeTruthy();
+
+    });
+    it('check obj is truthy', () => {
+        expect(validate.isTruthy(obj)).toBeTruthy();
+        expect(validate.isTruthy()).toBeFalsy();
     });
   
 });
